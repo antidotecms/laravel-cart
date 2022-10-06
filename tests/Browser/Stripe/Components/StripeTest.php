fa9f2bf54c2fdfd6f079959c02708bc8ee54be7b
Tim Smith <tim.smith@titan21.co.uk>
2022-10-06 15:44:33 +0100

    Update to dusk testing

--- tests/Browser/Stripe/Components/StripeTest.php
+++ tests/Browser/Stripe/Components/StripeTest.php
@@ -2,9 +2,11 @@
 
 namespace Antidote\LaravelCart\Tests\Browser\Stripe\Components;
 
+use Antidote\LaravelCart\Tests\laravel\app\Models\Products\TestCustomer;
 use Antidote\LaravelCart\Tests\laravel\app\Models\Products\TestProduct;
 use Illuminate\Foundation\Testing\Concerns\InteractsWithViews;
-use Orchestra\Testbench\Factories\UserFactory;
+use Illuminate\Support\Facades\DB;
+use Laravel\Dusk\Browser;
 
 class StripeTest extends \Antidote\LaravelCart\Tests\BrowserTestCase
 {
@@ -19,22 +21,29 @@ class StripeTest extends \Antidote\LaravelCart\Tests\BrowserTestCase
             'price' => 1000
         ])->create();
 
-//        $customer = TestCustomer::factory()->create();
-        $user = UserFactory::new()->create();
-        //DB::commit();
+        $user = TestCustomer::factory()->create();
+        //$user = UserFactory::new()->create();
+        //dump(get_class($user)); // "Illuminate\Foundation\Auth\User"
+        DB::commit();
 
-        dump(env('APP_ENV'));
+        //dump(env('APP_ENV'));
+        //dump($user);
 
-        $this->browse(function($browser) use ($user) {
+        $this->browse(function(Browser $browser) use ($user) {
 
             //$customer = TestCustomer::factory()->create();
 
+            //dump($user);
+
             $browser
+                //->visit("_dusk/login/".$user->id."/web")
                 //->actingAs($user, 'web')
-                ->loginAs($user, 'web')
+                ->loginAs($user->id, 'web')
                 //->assertAuthenticatedAs($user, 'web')
-                ->visit('/')
-                ->assertSee("home");
+                //->tap(fn() => sleep(10))
+                ->assertAuthenticatedAs($user, 'web')
+                ->visit('/user')
+                ->assertSee("hello");
                 //->assertUrlIs('http://127.0.0.1:8001/checkout');
 
 //            dump(Auth::user());
--- tests/BrowserTestCase.php
+++ tests/BrowserTestCase.php
@@ -11,6 +11,8 @@ use Antidote\LaravelCart\Tests\laravel\app\Models\TestOrderLogItem;
 use Antidote\LaravelCart\Tests\laravel\app\Models\TestPayment;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Support\Facades\Config;
+use Laravel\Dusk\DuskServiceProvider;
+use Orchestra\Testbench\Dusk\Foundation\TestbenchServiceProvider;
 
 //use WireUi\Providers\WireUiServiceProvider;
 
@@ -23,11 +25,17 @@ class BrowserTestCase extends \Orchestra\Testbench\Dusk\TestCase
 {
     use DatabaseTransactions;
 
-    protected static $baseServePort = 80;
+    //protected static $baseServePort = 80;
 
     protected function defineDatabaseMigrations()
     {
-        $this->artisan('migrate:fresh');
+        //$this->artisan('migrate:fresh');
+        $this->artisan('migrate:fresh', ['--database' => 'testbench'])->run();
+
+        $this->beforeApplicationDestroyed(function () {
+            $this->artisan('migrate:rollback', ['--database' => 'testbench'])->run();
+        });
+
         $this->loadMigrationsFrom(__DIR__.'/laravel/database/migrations');
         $this->loadLaravelMigrations(['--database' => 'testbench']);
     }
@@ -36,11 +44,15 @@ class BrowserTestCase extends \Orchestra\Testbench\Dusk\TestCase
     {
         parent::setUp();
 
-        //$this->loadLaravelMigrations();
+        $this->loadLaravelMigrations();
 
         $this->tweakApplication(function ($app, $config) {
             $app['router']->get('/checkout', fn() => view('checkout'));
-            $app['router']->get('/', fn() => "home");
+            $app['router']->get('/', fn() => "baweep");
+            $app['router']->get('/test', fn() => env('DB_CONNECTION'));
+            $app['router']->get('/user', fn() => print_r(auth()->user()->attributesToArray(), true));
+            $app['router']->get('/hello', fn() => "hello");
+            //$app['router']->get('_dusk/login/{userId}/{guard?}', [UserController::class, 'login'])->name('login');
             //$app['router']->get('/login', fn() => 'login')->name('dusk.login');
             //$app['router']->get('/user', fn() => 'user')->name('dusk.user');
         });
@@ -54,23 +66,40 @@ class BrowserTestCase extends \Orchestra\Testbench\Dusk\TestCase
 //            LivewireServiceProvider::class,
 //            FilamentServiceProvider::class
             //WireUiServiceProvider::class
+            DuskServiceProvider::class,
+            \Orchestra\Testbench\Foundation\TestbenchServiceProvider::class,
+            TestbenchServiceProvider::class
         ];
     }
 
     protected function getEnvironmentSetUp($app)
     {
-        //$this->loadFactoriesUsing($app, __DIR__.'/Fixtures/database/factories');
-
-        // Setup default database to use sqlite :memory:
         $app['config']->set('database.default', 'testbench');
         $app['config']->set('database.connections.testbench', [
             'driver'   => 'sqlite',
             'database' => __DIR__.'/laravel/database/database.sqlite.example',
             'prefix'   => '',
         ]);
+
+        //$app['config']->set('auth.password_timeout',10800);
+
+        //$app['config']->set('auth.defaults.guard', 'web');
+        //$app['config']->set('dusk.domain', 'localhost');
+        $app['config']->set('auth.guards', [
+            'web' => [
+                'driver' => 'session',
+                'provider' => 'test_customers',
+            ]
+        ]);
+        $app['config']->set('auth.providers', [
+            'test_customers' => [
+                'driver' => 'eloquent',
+                'model' => TestCustomer::class,
+            ]
+        ]);
         //$app['config']->set('database.connections.testbench', 'mysql');
 
-        $app['config']->set('session.driver', 'file');
+        //$app['config']->set('session.driver', 'file');
 
         Config::set('laravel-cart.classes.product', TestProduct::class);
         Config::set('laravel-cart.classes.customer', TestCustomer::class);
@@ -93,4 +122,24 @@ class BrowserTestCase extends \Orchestra\Testbench\Dusk\TestCase
     {
         return __DIR__.'/laravel';
     }
+
+//    protected function driver() : RemoteWebDriver
+//    {
+//        $options = (new ChromeOptions())->addArguments([
+//            //'--disable-gpu',
+//            //'--headless',
+//            //'--window-size=1920,1080',
+//            '--no-sandbox',
+//            '--enable-file-cookies',
+//        ]);
+//
+//        // I use Docker, container name is chrome.
+//        return RemoteWebDriver::create(
+//            'http://127.0.0.1:9515',
+//            DesiredCapabilities::chrome()->setCapability(
+//                ChromeOptions::CAPABILITY,
+//                $options
+//            )
+//        );
+//    }
 }
