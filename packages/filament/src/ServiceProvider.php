09554da6d4310381fc70b5e99b50f8b515254ff8
Tim Smith <tim.smith@titan21.co.uk>
2022-10-06 16:23:18 +0100

    Revert Filament service provider back

--- packages/filament/src/ServiceProvider.php
+++ packages/filament/src/ServiceProvider.php
@@ -2,23 +2,20 @@
 
 namespace Antidote\LaravelCartFilament;
 
+use Antidote\LaravelCartFilament\Resources\CustomerResource;
+use Antidote\LaravelCartFilament\Resources\OrderResource;
 use Filament\PluginServiceProvider;
 use Spatie\LaravelPackageTools\Package;
 
-//class ServiceProvider extends PluginServiceProvider
-//{
-//    protected array $resources = [
-//        OrderResource::class,
-//        CustomerResource::class,
-//    ];
-//
-//    public function configurePackage(Package $package): void
-//    {
-//        $package->name('laravel-cart-filament');
-//    }
-//}
-
-class ServiceProvider extends \Illuminate\Support\ServiceProvider
+class ServiceProvider extends PluginServiceProvider
 {
+    protected array $resources = [
+        OrderResource::class,
+        CustomerResource::class,
+    ];
 
+    public function configurePackage(Package $package): void
+    {
+        $package->name('laravel-cart-filament');
+    }
 }
