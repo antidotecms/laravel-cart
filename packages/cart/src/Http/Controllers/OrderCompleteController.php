72f4b6c6bfe84a44552a587c6892e15491fb9059
Tim Smith <tim.smith@titan21.co.uk>
2022-12-14 13:43:23 +0000

    Adding controller for order complete route

---
+++ packages/cart/src/Http/Controllers/OrderCompleteController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Antidote\LaravelCart\Http\Controllers;
+
+use Antidote\LaravelCart\Facades\Cart;
+
+class OrderCompleteController extends \Illuminate\Routing\Controller
+{
+    public function __invoke()
+    {
+        Cart::setActiveOrder(null);
+
+        if ($order_id = request()->get('order_id')) {
+
+            $order = getClassNameFor('order')::where('id', $order_id)->first();
+
+            if ($order && $order->customer->id == auth()->guard('customer')->user()->id) {
+                return view(config('laravel-cart.views.order_complete'), [
+                    'order' => $order,
+                    'completed' => $order->status == 'Charge Succeeded'
+                ]);
+            }
+        }
+
+        abort(404);
+    }
+}
