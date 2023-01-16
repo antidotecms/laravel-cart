717b25c11b99de9e326c0c4c96beeb8145c41fba
Tim Smith <tim.smith@titan21.co.uk>
2023-01-16 18:50:19 +0000

    Adding tests for order factory

---
+++ tests/Feature/Cart/Database/Factories/OrderFactoryTest.php
@@ -0,0 +1,32 @@
+<?php
+
+//order factory test
+use Antidote\LaravelCart\Tests\laravel\app\Models\Products\TestCustomer;
+use Antidote\LaravelCart\Tests\laravel\app\Models\Products\TestProduct;
+use Antidote\LaravelCart\Tests\laravel\app\Models\TestOrder;
+
+it('will correctly create an order with a simple product', function () {
+
+    $product = TestProduct::factory()->asSimpleProduct()->create();
+    $customer = TestCustomer::factory()->create();
+
+    $order = TestOrder::factory()
+        ->forCustomer($customer)
+        ->withProduct($product, 2)
+        ->create();
+
+    expect($order->getSubtotal())->toBe($product->getPrice() * 2);
+});
+
+it('will correctly create an order with a complex product', function () {
+
+    $product = TestProduct::factory()->asComplexProduct()->create();
+    $customer = TestCustomer::factory()->create();
+
+    $order = TestOrder::factory()
+        ->forCustomer($customer)
+        ->withProduct($product, 2, ['width' => 10, 'height' => 10])
+        ->create();
+
+    expect($order->getSubtotal())->toBe($product->getPrice() * 2)->toBe(200);
+});
