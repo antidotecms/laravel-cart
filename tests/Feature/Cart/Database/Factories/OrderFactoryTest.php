bb55d333a9ef8c1a938c6eab138a1d74e14e8d27
Tim Smith <tim.smith@titan21.co.uk>
2023-01-18 14:22:34 +0000

    Adding order factory test to ensure it can create a variable product

--- tests/Feature/Cart/Database/Factories/OrderFactoryTest.php
+++ tests/Feature/Cart/Database/Factories/OrderFactoryTest.php
@@ -30,3 +30,16 @@ it('will correctly create an order with a complex product', function () {
 
     expect($order->getSubtotal())->toBe($product->getPrice() * 2)->toBe(200);
 });
+
+it('will correctly create an order with a variable product', function() {
+
+    $product = TestProduct::factory()->asVariableProduct()->create();
+    $customer = TestCustomer::factory()->create();
+
+    $order = TestOrder::factory()
+        ->forCustomer($customer)
+        ->withProduct($product, 2, ['width' => 10, 'height' => 10])
+        ->create();
+
+    expect($order->getSubtotal())->toBe($product->getPrice(['width' => 10, 'height' => 10]) * 2)->toBe(200);
+});
