34abc2637ffca140f0677fc4fe32d6630d0dc060
Tim Smith <tim.smith@titan21.co.uk>
2023-02-24 21:02:00 +0000

    Removing factory

--- packages/cart/src/Contracts/OrderItem.php
+++ packages/cart/src/Contracts/OrderItem.php
@@ -3,20 +3,12 @@
 namespace Antidote\LaravelCart\Contracts;
 
 use Antidote\LaravelCart\Concerns\ConfiguresOrderItem;
-use Antidote\LaravelCart\Database\factories\OrderItemFactory;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 abstract class OrderItem extends Model
 {
     use ConfiguresOrderItem;
-    use HasFactory;
-
-    protected static function newFactory()
-    {
-        return OrderItemFactory::new();
-    }
 
     public function product(): BelongsTo
     {
