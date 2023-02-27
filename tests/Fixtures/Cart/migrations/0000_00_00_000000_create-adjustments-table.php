1e2ba6745c8603bba038355a9e2270c159bb00ea
Tim Smith <tim.smith@titan21.co.uk>
2023-02-27 13:08:46 +0000

    Adding additional columns to migration for adjustments

--- tests/Fixtures/Cart/migrations/0000_00_00_000000_create-adjustments-table.php
+++ tests/Fixtures/Cart/migrations/0000_00_00_000000_create-adjustments-table.php
@@ -12,6 +12,10 @@ return new class extends Migration {
             $table->string('name');
             $table->string('class');
             $table->json('parameters');
+            $table->boolean('apply_to_subtotal')->default(true);
+            $table->boolean('is_active')->default(true);
+            $table->boolean('is_valid')->default(true);
+            $table->boolean('calculated_amount')->default(0);
             $table->timestamps();
         });
     }
