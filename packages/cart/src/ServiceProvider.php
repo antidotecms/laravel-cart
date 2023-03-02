1b0c838af77188da7a4bb5c183f44152352c0d43
Tim Smith <tim.smith@titan21.co.uk>
2023-03-02 11:26:12 +0000

    Correcting bind of Order for OrderCompleteController

--- packages/cart/src/ServiceProvider.php
+++ packages/cart/src/ServiceProvider.php
@@ -2,7 +2,8 @@
 
 namespace Antidote\LaravelCart;
 
-use Antidote\LaravelCart\Contracts\Order;
+use Antidote\LaravelCart\Http\Controllers\OrderCompleteController;
+use Antidote\LaravelCart\Models\Order;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Config;
 
@@ -47,35 +48,23 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
 
     private function bindings()
     {
-        //dump(config('laravel-cart'));
-        //dump(config());
         $this->app->bind('cart', function () {
             return new \Antidote\LaravelCart\Domain\Cart();
         });
 
-        $this->app->bind(
-            Order::class,
-            function() {
+        $this->app->when(OrderCompleteController::class)
+            ->needs(Order::class)
+            ->give(function($app) {
                 if($order_id = request()->get('order_id')) {
                     $order = getClassNameFor('order')::where('id', $order_id)->first();
-                    if ($order && $order->customer->id == auth()->guard('customer')->user()->id) {
+                    if (auth('customer')->check() && $order && $order->customer->id == auth()->guard('customer')->user()->id) {
                         $order->load('items.product.productType');
                         return $order;
                     }
                 }
-            }
-        );
 
-//        $this->app->when(OrderCompleteController::class)
-//            ->needs(Order::class)
-//            ->give(function() {
-//                if($order_id = request()->get('order_id')) {
-//                    $order = getClassNameFor('order')::where('id', $order_id)->first()->load('items.product.productType');
-//                    if($order->customer->id == auth()->guard('customer')->user()->id) {
-//                        return $order;
-//                    }
-//                }
-//            });
+                return null;
+            });
     }
 
     private function configuration()
