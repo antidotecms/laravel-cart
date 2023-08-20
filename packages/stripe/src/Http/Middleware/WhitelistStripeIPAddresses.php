5a28095b8975ba225dd4f63e44811f4fe1e7d7fa
Tim Smith <tim.smith@titan21.co.uk>
2023-08-20 20:13:11 +0100

    Adding tests

--- packages/stripe/src/Http/Middleware/WhitelistStripeIPAddresses.php
+++ packages/stripe/src/Http/Middleware/WhitelistStripeIPAddresses.php
@@ -4,6 +4,7 @@ namespace Antidote\LaravelCartStripe\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Log;
 use Symfony\Component\HttpFoundation\IpUtils;
@@ -26,6 +27,15 @@ class WhitelistStripeIPAddresses
         //internal for use in docker
         "172.0.0.0/8"
     ];
+
+    public function __construct()
+    {
+        if(App::environment('local', 'development', 'testing'))
+        {
+            $this->ip_addresses[] = '127.0.0.1';
+        }
+    }
+
     /**
      * Handle an incoming request.
      *
@@ -33,21 +43,28 @@ class WhitelistStripeIPAddresses
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle(Request $request, Closure $next)
+    public function handle(Request $request, Closure $next): Response
     {
-        if(App::environment('local', 'development', 'testing'))
-        {
-            $this->ip_addresses[] = '127.0.0.1';
-        }
-
-        Log::info($request->getClientIp());
+        //Log::info($request->getClientIp());
         //if(!in_array($request->getClientIp(), $this->ip_addresses))
-        if(!IpUtils::checkIp($request->getClientIp(), $this->ip_addresses))
+        if(!$this->isValidIPAddress($request))
         {
-            Log::info('Stripe Attempt from IP: '.$request->getClientIp());
-            abort(403, "Unauthorized Access Stripe");
+            Log::info('Stripe Attempt from IP: '.($request->getClientIp() ?: 'Unknown'));
+            //abort(403, "Unauthorized Stripe Access");
+            return response("Unauthorized Stripe Access", 403);
         }
 
         return $next($request);
+    }
+
+    private function isValidIPAddress($request)
+    {
+        return $request->getClientIp() && IpUtils::checkIp($request->getClientIp(), $this->ip_addresses);
+    }
+
+    //@todo rethink this - adding this here to check that 127.0.0.1 was added when in a relevant environment
+    public function getWhitelistedIPAddresses(): array
+    {
+        return $this->ip_addresses;
     }
 }
--- tests/Feature/Stripe/Http/Middleware/WhitelistStripeAddressesTest.php
+++ tests/Feature/Stripe/Http/Middleware/WhitelistStripeAddressesTest.php
@@ -7,9 +7,11 @@ it('will restrict access to the web hook controller', function() {
             ->andReturn('62.30.207.45');
     });
 
-    (new \Antidote\LaravelCartStripe\Http\Middleware\WhitelistStripeIPAddresses())->handle($mockedRequest, function($request) {});
+    /** @var \Illuminate\Http\Response $response */
+    $response = (new \Antidote\LaravelCartStripe\Http\Middleware\WhitelistStripeIPAddresses())->handle($mockedRequest, function($request) {});
+    expect($response->getStatusCode())->toBe(403);
 })
-->expectExceptionMessage('Unauthorized Access Stripe')
+//->expectExceptionMessage('Unauthorized Stripe Access')
 ->coversClass(\Antidote\LaravelCartStripe\Http\Middleware\WhitelistStripeIPAddresses::class);
 
 it('will allow access to the web hook controller', function() {
@@ -19,8 +21,109 @@ it('will allow access to the web hook controller', function() {
             ->andReturn('3.18.12.63');
     });
 
-    $response = (new \Antidote\LaravelCartStripe\Http\Middleware\WhitelistStripeIPAddresses())->handle($mockedRequest, function($request) {  return true; });
+    $callback = response('', 200);
 
-    $this->assertTrue($response);
+    $response = (new \Antidote\LaravelCartStripe\Http\Middleware\WhitelistStripeIPAddresses())->handle($mockedRequest, fn() => $callback);
+
+    expect($response)->toBe($callback);
 })
 ->coversClass(\Antidote\LaravelCartStripe\Http\Middleware\WhitelistStripeIPAddresses::class);
+
+it('will log an entry if a stripe event was sent from an un-whitelisted ip address', function () {
+
+    \Illuminate\Support\Facades\Log::swap(new \TiMacDonald\Log\LogFake());
+
+    $mockedRequest = $this->partialMock(\Illuminate\Http\Request::class, function(\Mockery\MockInterface $mock) {
+        $mock->shouldReceive('getClientIp')
+            ->andReturn('62.30.207.45');
+    });
+
+        $response = (new \Antidote\LaravelCartStripe\Http\Middleware\WhitelistStripeIPAddresses())->handle($mockedRequest, function ($request) {
+        });
+
+        \Illuminate\Support\Facades\Log::assertLogged(function(\TiMacDonald\Log\LogEntry $log) {
+            return $log->message == 'Stripe Attempt from IP: 62.30.207.45';
+        });
+})
+->coversClass(\Antidote\LaravelCartStripe\Http\Middleware\WhitelistStripeIPAddresses::class);
+
+it('will log an entry if a stripe event was sent from an registered ip address as null', function () {
+
+    \Illuminate\Support\Facades\Log::swap(new \TiMacDonald\Log\LogFake());
+
+    $mockedRequest = $this->partialMock(\Illuminate\Http\Request::class, function(\Mockery\MockInterface $mock) {
+        $mock->shouldReceive('getClientIp')
+            ->andReturnNull();
+    });
+
+
+        $response = (new \Antidote\LaravelCartStripe\Http\Middleware\WhitelistStripeIPAddresses())->handle($mockedRequest, function ($request) {
+        });
+
+    \Illuminate\Support\Facades\Log::assertLogged(function(\TiMacDonald\Log\LogEntry $log) {
+        return $log->message == 'Stripe Attempt from IP: Unknown';
+    });
+
+})
+->coversClass(\Antidote\LaravelCartStripe\Http\Middleware\WhitelistStripeIPAddresses::class);
+
+it('will log an entry if a stripe event was sent from an registered ip address as empty string', function () {
+
+    \Illuminate\Support\Facades\Log::swap(new \TiMacDonald\Log\LogFake());
+
+    $mockedRequest = $this->partialMock(\Illuminate\Http\Request::class, function(\Mockery\MockInterface $mock) {
+        $mock->shouldReceive('getClientIp')
+            ->andReturn('');
+    });
+
+
+    $response = (new \Antidote\LaravelCartStripe\Http\Middleware\WhitelistStripeIPAddresses())->handle($mockedRequest, function ($request) {
+    });
+
+    \Illuminate\Support\Facades\Log::assertLogged(function(\TiMacDonald\Log\LogEntry $log) {
+        return $log->message == 'Stripe Attempt from IP: Unknown';
+    });
+
+})
+    ->coversClass(\Antidote\LaravelCartStripe\Http\Middleware\WhitelistStripeIPAddresses::class);
+
+it('will abort with a 403 response if a stripe event was sent from an un-whitelisted ip address', function () {
+
+    $mockedRequest = $this->partialMock(\Illuminate\Http\Request::class, function(\Mockery\MockInterface $mock) {
+        $mock->shouldReceive('getClientIp')
+            ->andReturn('62.30.207.45');
+    });
+
+    //$this->expectException(\Symfony\Component\HttpKernel\Exception\HttpException::class);
+    //$this->expectExceptionMessage('Unauthorized Stripe Access');
+
+    try {
+        $response = (new \Antidote\LaravelCartStripe\Http\Middleware\WhitelistStripeIPAddresses())->handle($mockedRequest, function ($request) {
+        });
+    } catch(\Symfony\Component\HttpKernel\Exception\HttpException $e) {
+        expect($e->getStatusCode())->toBe(403);
+    }
+
+    //expect($response->getStatusCode())->toBe(403);
+})
+->coversClass(\Antidote\LaravelCartStripe\Http\Middleware\WhitelistStripeIPAddresses::class);
+
+it('will not add localhost as a trusted IP when developing', function () {
+
+    //app()->runningUnitTests(fn() => true);
+
+    app()->detectEnvironment(fn() => 'production');
+
+    $mockedRequest = $this->partialMock(\Illuminate\Http\Request::class, function(\Mockery\MockInterface $mock) {
+
+        $mock->shouldReceive('getClientIp')
+            ->andReturn('62.30.207.45');
+    });
+
+    $middleware = new \Antidote\LaravelCartStripe\Http\Middleware\WhitelistStripeIPAddresses();
+
+    //$middleware->handle($mockedRequest, function($request) {});
+
+    expect($middleware->getWhitelistedIPAddresses())->not()->toContain('127.0.0.1');
+})
+->skip('unsure how to test this. Need to be able to change APP_ENV temporarily for this one test');
