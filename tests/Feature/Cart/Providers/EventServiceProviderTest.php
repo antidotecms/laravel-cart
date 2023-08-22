0ba3a2fceb8d0bace1f4a9ce0db56fb16435f52b
Tim Smith <tim.smith@titan21.co.uk>
2023-08-22 19:51:42 +0100

    Adding tests

---
+++ tests/Feature/Cart/Providers/EventServiceProviderTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Antidote\LaravelCart\Tests\Feature\Cart\Providers;
+
+use Antidote\LaravelCart\Events\OrderCompleted;
+use Antidote\LaravelCart\Listeners\SendOrderConfirmation;
+use Antidote\LaravelCart\Providers\EventServiceProvider;
+use Antidote\LaravelCart\ServiceProvider;
+use Antidote\LaravelCart\Tests\TestCase;
+use Illuminate\Support\Facades\Event;
+
+/**
+ * @covers \Antidote\LaravelCart\Providers\EventServiceProvider
+ */
+class EventServiceProviderTest extends TestCase
+{
+    protected function getPackageProviders($app): array
+    {
+        return [
+            ServiceProvider::class,
+            EventServiceProvider::class
+        ];
+    }
+
+    /**
+     * @test
+     */
+    public function it_uses_the_correct_class()
+    {
+        Event::fake();
+        Event::assertListening(OrderCompleted::class, SendOrderConfirmation::class);
+    }
+}
