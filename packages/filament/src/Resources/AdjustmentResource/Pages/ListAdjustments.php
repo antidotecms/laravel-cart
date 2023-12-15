110429ec4ec2a34d2fa453902c08122f20b9c42c
Tim Smith <tim.smith@titan21.co.uk>
2023-12-15 12:36:17 +0000

    Adding actions to add products and adjustments

--- packages/filament/src/Resources/AdjustmentResource/Pages/ListAdjustments.php
+++ packages/filament/src/Resources/AdjustmentResource/Pages/ListAdjustments.php
@@ -3,9 +3,17 @@
 namespace Antidote\LaravelCartFilament\Resources\AdjustmentResource\Pages;
 
 use Antidote\LaravelCartFilament\Resources\AdjustmentResource\Pages\Concerns\ConfiguresAdjustmentResourcePages;
+use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 
 class ListAdjustments extends ListRecords
 {
     use ConfiguresAdjustmentResourcePages;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            CreateAction::make()
+        ];
+    }
 }
--- packages/filament/src/Resources/ProductResource/Pages/ListProducts.php
+++ packages/filament/src/Resources/ProductResource/Pages/ListProducts.php
@@ -3,9 +3,17 @@
 namespace Antidote\LaravelCartFilament\Resources\ProductResource\Pages;
 
 use Antidote\LaravelCartFilament\Resources\ProductResource\Pages\Concerns\ConfiguresProductResourcePages;
+use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 
 class ListProducts extends ListRecords
 {
     use ConfiguresProductResourcePages;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            CreateAction::make()
+        ];
+    }
 }
