7c1d57a7b23301ce2294bc757e26c6398316892d
Tim Smith <tim.smith@titan21.co.uk>
2024-01-17 20:00:15 +0000

    Removing redundant code

--- packages/cart/src/Listeners/SendOrderConfirmation.php
+++ packages/cart/src/Listeners/SendOrderConfirmation.php
@@ -4,27 +4,11 @@ namespace Antidote\LaravelCart\Listeners;
 
 use Antidote\LaravelCart\Events\OrderCompleted;
 use Filament\Notifications\Notification;
-use Illuminate\Support\Facades\Validator;
 
 class SendOrderConfirmation
 {
     public function handle(OrderCompleted $event)
     {
-        $order_complete_mail = config('laravel-cart.classes.mails.order_complete') ?? OrderComplete::class;
-        $order_complete_recipient_email =  config('laravel-cart.emails.order_complete');
-
-        $validator = Validator::make(['email' => $order_complete_recipient_email], [
-            'email' => 'email|nullable'
-        ]);
-
-        if($validator->fails()) {
-            $order_complete_recipient_email = null;
-        }
-
-//        Mail::to($event->order->customer)
-//            ->bcc($order_complete_recipient_email)
-//            ->send(new $order_complete_mail($event->order));
-
         $event->order->customer->notify(new \Antidote\LaravelCart\Notifications\OrderComplete($event->order));
 
         $event->order->log('Order complete mail sent to '.$event->order->customer->email);
